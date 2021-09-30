8512c569abed8f74802be65e6f0d08568c85be31
sakuraseed <hawky005@gmail.com>
2021-09-30 19:41:25 +0300

    add requirements.txt

--- utopia/settings.py
+++ utopia/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.storage.CompressedManifestStaticFilesStorage'
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
