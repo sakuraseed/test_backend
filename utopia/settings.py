f5bd53a6d982423f3e63779d8683561f4a105fc1
sakuraseed <hawky005@gmail.com>
2021-09-30 22:25:26 +0300

    add requirements.txt

--- utopia/settings.py
+++ utopia/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
